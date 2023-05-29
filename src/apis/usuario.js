8adc0e2892ac9ecc0fb0988b0a5f888ec8ef1eaf
LizethLorenaSandoval <lizeth.sandoval@btgpactual.com>
2023-05-29 16:43:22 -0500

    se modifica api de traer usuarios

--- src/apis/usuario.js
+++ src/apis/usuario.js
@@ -5,7 +5,7 @@ const mySqlConnection = require("../conexion");
 //? Traer todos los datos de la tabla usuarios ============================================
 router.get("/usuario", (req, res) => {
     const query =
-      "select id_usuario, concat(nombre, ' ',apellido) as Nombre, id_estado_usuario, documento, id_celula, correo, id_rol from usuario";
+      "select u.id_usuario, concat(u.nombre, ' ',u.apellido) as Nombre, u.id_estado_usuario, td.nombre_tipo_documento, u.documento, c.nombre_celula, u.correo, r.nombre_rol from usuario u inner join tipo_documento td on td.id_tipo_documento = u.id_tipo_documento inner join celula c on c.id_celula = u.id_celula inner join rol r on r.id_rol = u.id_rol";
     mySqlConnection.query(query, (err, rows, fields) => {
       if (!err) {
         res.send(rows);
