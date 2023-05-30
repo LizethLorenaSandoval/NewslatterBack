b881165a0a8ed2202f3b26da8c08a05f36cb3618
LizethLorenaSandoval <lizeth.sandoval@btgpactual.com>
2023-05-30 09:54:16 -0500

    se modifica api de roles

--- src/apis/rol.js
+++ src/apis/rol.js
@@ -5,7 +5,7 @@ const mySqlConnection = require("../conexion");
 //? Traer todos los datos de la tabla rol ============================================
 router.get("/rol", (req, res) => {
     const query =
-      "select * from rol";
+      "select r.id_rol, r.nombre_rol, er.nombre_estado_rol from rol r inner join estado_rol er on er.id_estado_rol = r.id_rol";
     mySqlConnection.query(query, (err, rows, fields) => {
       if (!err) {
         res.send(rows);
