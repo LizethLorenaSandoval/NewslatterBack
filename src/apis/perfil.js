59d27aa117bd04b710450995f8ebe658e595fd96
LizethLorenaSandoval <lizeth.sandoval@btgpactual.com>
2023-07-13 16:00:24 -0500

    se hace api para editar perfil

--- src/apis/perfil.js
+++ src/apis/perfil.js
@@ -7,7 +7,7 @@ router.get("/miperfil/:id_usuario", (req, res) => {
 
     const { id_usuario } = req.params;
     mySqlConnection.query(
-      "SELECT u.id_usuario, u.nombre, u.apellido, u.correo, u.documento, c.nombre_celula, r.nombre_rol, td.nombre_tipo_documento FROM `usuario` u inner join celula c on c.id_celula = u.id_celula inner join rol r on r.id_rol = u.id_rol inner join tipo_documento td on td.id_tipo_documento = u.id_tipo_documento WHERE id_usuario = ?",
+      "SELECT u.id_usuario, u.nombre, u.apellido, u.correo, u.documento, c.id_celula, c.nombre_celula, r.nombre_rol, td.id_tipo_documento, td.nombre_tipo_documento FROM `usuario` u inner join celula c on c.id_celula = u.id_celula inner join rol r on r.id_rol = u.id_rol inner join tipo_documento td on td.id_tipo_documento = u.id_tipo_documento WHERE id_usuario = ?",
       [id_usuario],
       (err, rows, fields) => {
         if (!err) {
@@ -19,6 +19,23 @@ router.get("/miperfil/:id_usuario", (req, res) => {
               statusCode: 403,
             });
           }
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
+
+  //?Editar mi perfil =====================================================================================
+router.put("/editarperfil/:id_usuario", (req, res) => {
+    const { nombre, apellido, id_celula, id_tipo_documento } = req.body;
+    const { id_usuario } = req.params;
+    mySqlConnection.query(
+      "UPDATE usuario SET nombre = ?, apellido = ?, id_celula = ?, id_tipo_documento = ? WHERE id_usuario = ?",
+      [nombre, apellido, id_celula, id_tipo_documento, id_usuario],
+      (err, rows, fields) => {
+        if (!err) {
+          res.json({ status: "Usuario actualizado" });
         } else {
           console.log(err);
         }
