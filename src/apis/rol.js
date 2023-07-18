8210cf4913ccd5ea0b280cfa61498a6fead15d55
LizethLorenaSandoval <lizeth.sandoval@btgpactual.com>
2023-07-18 15:02:13 -0500

    se hace api para eliminar rol

--- src/apis/rol.js
+++ src/apis/rol.js
@@ -39,5 +39,38 @@ router.post("/crear_rol", (req, res) => {
     );
   });  
 
+//?Traer por id de un rol =====================================================================================
+  router.get("/rolid/:id_rol", (req, res) => {
+    const { id_rol } = req.params;
+    query = "select r.id_rol, r.nombre_rol, er.nombre_estado_rol from rol r inner join estado_rol er on er.id_estado_rol = r.estado_rol WHERE id_rol = ?"
+    mySqlConnection.query(
+      query,
+      [id_rol],
+      (err, rows, fields) => {
+        if (!err) {
+          res.send(rows);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
+
+
+//?Eliminar un rol =====================================================================================
+router.delete("/eliminarrol/:id", (req, res) => {
+  const { id_rol } = req.params;
+  mySqlConnection.query(
+    "DELETE FROM rol WHERE id_rol = ?",
+    [id_rol],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ status: "Rol eliminado" });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
 
 module.exports = router;
